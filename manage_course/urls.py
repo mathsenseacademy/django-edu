0f147b926544b65639c9123370039a133f755d6a
Abhishek Nath <oovnath2@gmail.com>
2025-05-29 23:03:12 +0530

    push course urls

--- manage_course/urls.py
+++ manage_course/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     #batch related urls
-    path('createbatches/', views.batches, name='create_batches'),
-    path('editbatches/', views.editbatchs, name='edit_batches'),
+    path('create_course/', views.create_course, name='create_course'),
+    path('edit_course/', views.edit_course, name='edit_course'),
+    path('showcourseid/', views.show_course_by_id, name='show_course_by_id'),
+    path('showallcourse/', views.show_all_courses, name='show_all_batch'),
+    path('showallactivatecourse/', views.show_all_activate_courses, name='show_all_activate_batch'),
 
     #course related urls
-    path('createcourses/', views.createcourses, name='create_courses'),
-    path('editcourses/', views.editcourses, name='edit_courses'),
+    # path('createcourses/', views.createcourses, name='create_courses'),
+    # path('editcourses/', views.editcourses, name='edit_courses'),
 ]
