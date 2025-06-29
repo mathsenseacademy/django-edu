29277f4571f1cd7002fffd7a838b377b0513c6e8
Abhishek Nath <oovnath2@gmail.com>
2025-06-29 22:28:12 +0530

    student login

--- django_edu_proj/settings.py
+++ django_edu_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,6 +74,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'administrator.authentication.AdminJWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 AUTH_USER_MODEL = 'administrator.Administrator'
--- student_user/urls.py
+++ student_user/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from .views import  student_register_request_otp, student_confirm_otp, student_login, student_register,student_list,verified_student_list,student_detail_by_id, student_register_verify_otp,update_student_detail
-
+# from .views import  student_register_request_otp, student_confirm_otp, student_login, student_register,student_list,verified_student_list,student_detail_by_id, student_register_verify_otp,update_student_detail ,request_student_login_otp
+from student_user import views
 urlpatterns = [
     # path('register/', student_register),
-    path('register/', student_register_request_otp),
+    path('register/', views.student_register_request_otp),
     # path('sendotp/', send_otp),
-    path('otpverify/', student_register_verify_otp),
+    path('otpverify/', views.student_register_verify_otp),
     # path('otpverify/', student_confirm_otp),
-    path('login/', student_login),
+    path('login/', views.student_login),
     # path('register/', student_register),
-    path('student_list/', student_list),
-    path('verified_student_list/', verified_student_list),
-    path('student_detail_by_id/', student_detail_by_id),
-    path('update_student_detail/', update_student_detail),
+    path('student_list/', views.student_list),
+    path('verified_student_list/', views.verified_student_list),
+    path('student_detail_by_id/', views.student_detail_by_id),
+    path('update_student_detail/', views.update_student_detail),
+    path('request_student_login_otp/', views.request_student_login_otp),
+    path('verify_student_login_otp/', views.verify_student_login_otp),
 ]
